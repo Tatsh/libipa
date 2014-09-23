0088807d18a246985f92a88b444fa381c56885f4
Andrew Udvare <audvare@gmail.com>
2014-09-23 02:44:34 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='libipa',
-    version='0.0.2',
+    version='0.0.3',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     packages=['ipa'],
