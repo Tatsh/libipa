9be0622ff7b89749753b3927be9eeeafed2e4ddb
0x70ad57001 <0x70ad57001@gmail.com>
2015-01-10 10:53:20 -0500

    More minor edits, this also brings this branch up-to-date with the current branch of Tatsh's repo.

--- ipa/test/__init__.py
+++ ipa/test/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
 from os import remove as rm
 from tempfile import mkstemp
@@ -11,10 +10,7 @@ import unittest
 from biplist import writePlistToString
 import six
 
-try:
-    from ipa import BadIPAError, IPAFile
-except ImportError:
-    execfile('../__init__.py')
+from ipa import BadIPAError, IPAFile
 
 class TestIPAFile(unittest.TestCase):
     _temp_files = []
@@ -27,38 +23,64 @@ class TestIPAFile(unittest.TestCase):
 
         return ''.join(ret)
 
-    def _create_ipa(self, create_info_plist=True, universal=False, app_name=None):
+    def _create_ipa(self, create_info_plist=True, universal=False, 
+app_name=None, bundle_display_name=None):
         h, zippath = mkstemp(prefix='libipa-', suffix='.ipa')
         self._temp_files.append(zippath)
-        app_dir = '%s.app' % (self._random_string(),) if not app_name else app_name
+
+        if not app_name:
+            app_name = self._random_string()
+        if six.PY2:
+            app_dir = b'%s.app' % (app_name,)
+        else:
+            app_dir = '{0}.app'.format(app_name).encode('utf-8')
 
         with ZipFile(zippath, 'w', ZIP_DEFLATED) as h:
             if create_info_plist:
                 info = dict(
-                    CFBundleIdentifier='com.%s.%s' % (self._random_string(), self._random_string(),),
-                    CFBundleDisplayName=self._random_string(),
+                    CFBundleIdentifier='com.%s.%s' % (self._random_string(), 
+self._random_string(),),
+                    CFBundleDisplayName=app_name,
                     LSRequiresIPhoneOS=True,
                     MinimumOSVersion='6.0',
                     UIStatusBarStyle='UIStatusBarStyleDefault',
                 )
+
+                if six.PY3 and type(app_dir) is str:
+                    app_dir = app_dir.encode('utf-8')
+                if six.PY3 and type(app_name) is str:
+                    app_name = app_name.encode('utf-8')
+
+                app_dir = b'Payload/' + app_dir + b'/'
+                app_path = app_dir + app_name
+
+                if six.PY3:
+                    app_path = app_path.decode('utf-8')
+
+                h.writestr(app_path, b'FACE')
+
+                if bundle_display_name is False:
+                    del info['CFBundleDisplayName']
 
                 if universal:
                     info['UIDeviceFamily'] = [1, 2]
                 else:
                     info['UIDeviceFamily'] = [1]
 
-                if six.PY3 and type(app_dir) is str:
-                    app_dir = app_dir.encode('utf-8')
+                info_plist_name = app_dir + b'Info.plist'
 
-                info_plist_name = (b'Payload/' + app_dir + b'/Info.plist').decode('utf-8')
+                if six.PY3:
+                    info_plist_name = info_plist_name.decode('utf-8')
 
                 h.writestr(info_plist_name, writePlistToString(info))
-                h.writestr('iTunesMetadata.plist', writePlistToString({'Test': 'Data'}))
+                h.writestr('iTunesMetadata.plist', writePlistToString({'Test': 
+'Data'}))
 
         return zippath
 
     def test_bad_ipa(self):
-        self.assertRaises(BadIPAError, IPAFile, self._create_ipa(create_info_plist=False))
+        self.assertRaises(BadIPAError, IPAFile, 
+self._create_ipa(create_info_plist=False))
 
     def test_ipa_info(self):
         ipa = IPAFile(self._create_ipa())
@@ -75,7 +97,7 @@ class TestIPAFile(unittest.TestCase):
             self.assertIn(k, ipa.app_info)
 
     def test_unicode_app_name(self):
-        ipa = IPAFile(self._create_ipa(app_name='ありがとう你好مرحبا.app'.encode('utf-8')))
+        ipa = IPAFile(self._create_ipa(app_name='ありがとう你好مرحبا'.encode('utf-8')))
         keys = (
             'CFBundleIdentifier',
             'CFBundleDisplayName',
@@ -109,6 +131,26 @@ class TestIPAFile(unittest.TestCase):
     def test_ipa_universal(self):
         ipa = IPAFile(self._create_ipa(universal=True))
         self.assertGreater(len(ipa.app_info['UIDeviceFamily']), 1)
+
+    def test_get_bin_name_normal(self):
+        ipa = IPAFile(self._create_ipa(app_name='A Name'))
+        self.assertEqual('A Name', ipa.get_bin_name())
+
+    def test_get_bin_name_no_display_name(self):
+        ipa = IPAFile(self._create_ipa(app_name='A Name', 
+bundle_display_name=False))
+        self.assertEqual('A Name', ipa.get_bin_name())
+
+    def test_get_bin_name_full(self):
+        ipa = IPAFile(self._create_ipa(app_name='A Name'))
+        self.assertEqual('Payload/A Name.app/A Name', 
+ipa.get_bin_name(full=True))
+
+    def test_get_bin_name_full_no_display_name(self):
+        ipa = IPAFile(self._create_ipa(app_name='A Name', 
+bundle_display_name=False))
+        self.assertEqual('Payload/A Name.app/A Name', 
+ipa.get_bin_name(full=True))
 
     def tearDown(self):
         for z in self._temp_files:
